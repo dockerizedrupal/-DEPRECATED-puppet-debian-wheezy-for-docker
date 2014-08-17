7328f08a3a1ced4cfee40054e84ef40e80129f0c
Jürgen Viljaste <j.viljaste@gmail.com>
2014-08-18 00:12:58 +0300

    Update

--- src/build/build.sh
+++ src/build/build.sh
@@ -20,8 +20,8 @@ apt-get -y install puppet
 # https://tickets.puppetlabs.com/browse/PUP-2566
 sed -i '/templatedir=\$confdir\/templates/d' /etc/puppet/puppet.conf
 
+# http://projects.puppetlabs.com/projects/puppet/wiki/Environment_Variables_Patterns
 mkdir -p /etc/puppet/modules/customfacts/lib/facter
-
 cp /opt/build/etc/puppet/modules/customfacts/lib/facter/env.rb /etc/puppet/modules/customfacts/lib/facter/env.rb
 
 rm /tmp/$PACKAGE
