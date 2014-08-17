21630a401bc1607d4dac12632ad5ff35fc3b52ad
Jürgen Viljaste <j.viljaste@gmail.com>
2014-08-17 21:03:14 +0300

    Update

--- src/build/build.sh
+++ src/build/build.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
 apt-get update
-apt-get -y install lsb-release openssl ca-certificates wget
+
+apt-get -y install lsb-release
+apt-get -y install openssl
+apt-get -y install ca-certificates
+apt-get -y install wget
 
 which /usr/bin/lsb_release
 
@@ -12,6 +16,7 @@ wget https://apt.puppetlabs.com/$PACKAGE -O /tmp/$PACKAGE
 dpkg -i /tmp/$PACKAGE
 
 apt-get update
+
 apt-get -y install puppet
 
 rm /tmp/$PACKAGE
