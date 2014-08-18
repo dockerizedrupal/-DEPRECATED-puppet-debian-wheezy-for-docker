2319105250557d7c073439ceb122b8869c1f749f
Jürgen Viljaste <j.viljaste@gmail.com>
2014-08-19 00:01:00 +0300

    Update

--- src/build/build.sh
+++ src/build/build.sh
@@ -2,10 +2,10 @@
 
 apt-get update
 
-apt-get -y install lsb-release
-apt-get -y install openssl
-apt-get -y install ca-certificates
-apt-get -y install wget
+apt-get install -y lsb-release
+apt-get install -y openssl
+apt-get install -y ca-certificates
+apt-get install -y wget
 
 PACKAGE=puppetlabs-release-$(lsb_release -sc).deb
 
@@ -15,7 +15,7 @@ dpkg -i /tmp/$PACKAGE
 
 apt-get update
 
-apt-get -y install puppet
+apt-get install -y puppet
 
 # https://tickets.puppetlabs.com/browse/PUP-2566
 sed -i '/templatedir=\$confdir\/templates/d' /etc/puppet/puppet.conf
@@ -23,5 +23,7 @@ sed -i '/templatedir=\$confdir\/templates/d' /etc/puppet/puppet.conf
 # http://projects.puppetlabs.com/projects/puppet/wiki/Environment_Variables_Patterns
 mkdir -p /etc/puppet/modules/customfacts/lib/facter
 cp /tmp/build/etc/puppet/modules/customfacts/lib/facter/env.rb /etc/puppet/modules/customfacts/lib/facter/env.rb
+
+cp /tmp/build/etc/puppet/hiera.yaml /etc/puppet/hiera.yaml
 
 rm /tmp/$PACKAGE
