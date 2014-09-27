df1523d06601066beb5244aee218e245a947217f
Jürgen Viljaste <j.viljaste@gmail.com>
2014-09-27 22:24:08 +0300

    Update

--- build/build.sh
+++ build/build.sh
@@ -9,9 +9,9 @@ apt-get install -y wget
 
 PACKAGE=puppetlabs-release-$(lsb_release -sc).deb
 
-wget https://apt.puppetlabs.com/"${PACKAGE}" -O /tmp/"${PACKAGE}"
+wget "https://apt.puppetlabs.com/${PACKAGE}" -O "/tmp/${PACKAGE}"
 
-dpkg -i /tmp/"${PACKAGE}"
+dpkg -i "/tmp/${PACKAGE}"
 
 apt-get update
 
@@ -19,10 +19,6 @@ apt-get install -y puppet
 
 # https://tickets.puppetlabs.com/browse/PUP-2566
 sed -i '/templatedir=\$confdir\/templates/d' /etc/puppet/puppet.conf
-
-# http://projects.puppetlabs.com/projects/puppet/wiki/Environment_Variables_Patterns
-mkdir -p /etc/puppet/modules/customfacts/lib/facter
-cp /tmp/build/etc/puppet/modules/customfacts/lib/facter/env.rb /etc/puppet/modules/customfacts/lib/facter/env.rb
 
 cp /tmp/build/etc/puppet/hiera.yaml /etc/puppet/hiera.yaml
 
--- build/etc/puppet/modules/customfacts/lib/facter/env.rb
+++
@@ -1,7 +0,0 @@
-ENV.each do |k,v|
-  Facter.add("env_#{k.downcase}".to_sym) do
-    setcode do
-      v
-    end
-  end
-end
