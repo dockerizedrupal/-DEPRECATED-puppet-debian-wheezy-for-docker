0fb3a69e1dd2aea1a9f8d481635376629434b98d
Jürgen Viljaste <j.viljaste@gmail.com>
2014-12-18 14:32:43 +0200

    Update

--- src/build/modules/puppet/manifests/init.pp
+++ src/build/modules/puppet/manifests/init.pp
@@ -4,8 +4,13 @@ class puppet {
     source => 'puppet:///modules/puppet/etc/puppet/hiera.yaml'
   }
 
+  exec { 'mkdir -p /etc/puppet/parser/functions':
+    path => ['/bin']
+  }
+
   file { '/etc/puppet/parser/functions/file_exists.rb':
     ensure => present,
-    source => 'puppet:///modules/puppet/etc/puppet/parser/functions/file_exists.rb'
+    source => 'puppet:///modules/puppet/etc/puppet/parser/functions/file_exists.rb',
+    require => Exec['mkdir -p /etc/puppet/parser/functions']
   }
 }
