514f1421dab6a718d0a127fb2ab6f64d67cc15fd
Jürgen Viljaste <j.viljaste@gmail.com>
2014-12-21 01:41:56 +0200

    Update

--- src/build/modules/puppet/manifests/init.pp
+++ src/build/modules/puppet/manifests/init.pp
@@ -1,4 +1,10 @@
 class puppet {
+  file { '/etc/apt/sources.list.d/contrib.list':
+    ensure => present,
+    source => 'puppet:///modules/puppet/etc/apt/sources.list.d/contrib.list',
+    mode => 644
+  }
+
   file { '/etc/puppet/hiera.yaml':
     ensure => present,
     source => 'puppet:///modules/puppet/etc/puppet/hiera.yaml'
