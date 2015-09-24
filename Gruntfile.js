5c17617f5177d0ad6018ba4cf8897d661623492a
Jürgen Viljaste <j.viljaste@gmail.com>
2015-09-24 19:59:48 +0300

    Update

---
+++ Gruntfile.js
@@ -0,0 +1,91 @@
+var current_version = '1.0.4';
+var new_version = '1.0.5';
+
+module.exports = function(grunt) {
+  grunt.loadNpmTasks('grunt-replace');
+
+  grunt.initConfig({
+    replace: {
+      task1: {
+        options: {
+          patterns: [
+            {
+              match: current_version,
+              replacement: new_version
+            }
+          ],
+          usePrefix: false
+        },
+        files: [
+          {
+            expand: true,
+            src: [
+              'VERSION.md'
+            ]
+          }
+        ]
+      },
+      task2: {
+        options: {
+          patterns: [
+            {
+              match: 'dockerizedrupal/puppet-debian-wheezy:' + current_version,
+              replacement: 'dockerizedrupal/puppet-debian-wheezy:' + new_version
+            }
+          ],
+          usePrefix: false
+        },
+        files: [
+          {
+            expand: true,
+            src: [
+              'README.md',
+              'tests/puppet-debian-wheezy.yml'
+            ]
+          }
+        ]
+      },
+      task3: {
+        options: {
+          patterns: [
+            {
+              match: 'git checkout ' + current_version,
+              replacement: 'git checkout ' + new_version
+            }
+          ],
+          usePrefix: false
+        },
+        files: [
+          {
+            expand: true,
+            src: [
+              'README.md'
+            ]
+          }
+        ]
+      },
+      task4: {
+        options: {
+          patterns: [
+            {
+              match: '"version": "' + current_version + '"',
+              replacement: '"version": "' + new_version + '"'
+            }
+          ],
+          usePrefix: false
+        },
+        files: [
+          {
+            expand: true,
+            src: [
+              'package.json'
+            ]
+          }
+        ]
+      }
+    }
+  });
+
+  grunt.registerTask('bump', 'replace');
+  grunt.registerTask('default', 'replace');
+};
--- README.md
+++ README.md
@@ -5,8 +5,8 @@
     TMP="$(mktemp -d)" \
       && git clone https://github.com/dockerizedrupal/docker-puppet-debian-wheezy.git "${TMP}" \
       && cd "${TMP}" \
-      && git checkout 1.0.4 \
-      && sudo docker build -t dockerizedrupal/puppet-debian-wheezy:1.0.4 . \
+      && git checkout 1.0.5 \
+      && sudo docker build -t dockerizedrupal/puppet-debian-wheezy:1.0.5 . \
       && cd -
 
 ## License
