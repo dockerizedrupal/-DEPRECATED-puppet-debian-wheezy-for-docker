f435142ec06ca1947c6191222f494e093708eeb5
Jürgen Viljaste <j.viljaste@gmail.com>
2015-09-24 20:18:10 +0300

    Update

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,5 @@
-var current_version = '1.0.4';
-var new_version = '1.0.5';
+var current_version = '1.0.5';
+var new_version = '1.0.6';
 
 module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-replace');
